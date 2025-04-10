0bb495bad29220c44521c1bfbca49914d4dedb19
CasiopeaChoi <asdweqw@gmail.com>
2025-04-11 02:51:44 +0900

    sln
    sln

---
+++ MHZ_Tester/clean-build.bat
@@ -0,0 +1,15 @@
+@echo off
+echo ===========================================
+echo     Deleting all bin / obj folders...
+echo ===========================================
+
+for /d /r %%d in (bin,obj) do (
+    if exist "%%d" (
+        echo Deleting : %%d
+        rmdir /s /q "%%d"
+    )
+)
+
+echo -------------------------------------------
+echo Done! Press any key to exit...
+pause >nul
